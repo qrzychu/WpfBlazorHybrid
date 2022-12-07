3c19a2fa28770e57b245fb60e415eb30de881a11
Krzysztof Skowronek <k.skowronek@mamoko.com>
2022-12-08 00:36:45 +0100

    Initial commit

---
+++ Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SkiaSharp;
+
+namespace Wpf.Blazor;
+
+public static class Extensions
+{
+    public static string? ToBase64Image(this SKBitmap bmp)
+    {
+        using var image = SKImage.FromBitmap(bmp);
+        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+
+        return "data:image/png;base64," + Convert.ToBase64String(data.ToArray());
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace Wpf.Blazor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+            serviceCollection.AddMudServices();
+            
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
